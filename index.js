5b80fbf62f6479bae49b457d1d99413f3ebc3a70
Suprio-Das <priyamd507@gmail.com>
2025-07-23 22:26:15 +0600

    Middlewares used

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
 dotenv.config()
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
+
+
+// Middlewares
+app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send("RBAC server is running!!!");
