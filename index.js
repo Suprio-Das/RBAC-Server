9c708f857afd16959004c3cc0a3084373a09a7e9
Suprio-Das <priyamd507@gmail.com>
2025-07-23 22:16:20 +0600

    dotenv configured

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config()
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send("RBAC server is running!!!");
