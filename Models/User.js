87c403825b21fb26ca21872adff8de501c307515
Suprio-Das <priyamd507@gmail.com>
2025-07-27 22:56:21 +0600

    User model created

--- Models/User.js
+++ Models/User.js
@@ -1,14 +1,24 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     role: {
         type: String,
-        default: "user"
+        enum: ['admin', 'user'],
+        default: 'user'
     }
-})
+}, { timestamps: true })
 
-const UserModel = mongoose.model("users", UserSchema);
-module.exports = UserModel;
+const UserModel = mongoose.model("user", UserSchema);
+export default UserModel;
