c609544aa3e4df592dc745555266495d8b7322f5
Suprio-Das <priyamd507@gmail.com>
2025-07-28 22:19:10 +0600

    password hashed and newUser model created

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,12 +1,23 @@
 import UserModel from "../Models/User.js";
+import bcrypt from 'bcrypt';
 
 export const register = async (req, res) => {
-    const { name, email, password } = req.body;
+    try {
+        const { name, email, password } = req.body;
 
-    // Check user existence
-    const existUser = await UserModel.findOne({ email: email });
-    if (existUser) {
-        return res.status(400).json({ success: false, message: "User already exist." })
+        // Check user existence
+        const existUser = await UserModel.findOne({ email: email });
+        if (existUser) {
+            return res.status(400).json({ success: false, message: "User already exist." })
+        }
+
+        const hashedPassword = bcrypt(password, 10);
+
+        const newUser = new UserModel({
+            name, email, password: hashedPassword
+        })
+
+    } catch (error) {
+        res.send(error);
     }
-    console.log("User is new");
 }
