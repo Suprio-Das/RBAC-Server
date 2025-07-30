a38023595c9e52854609ea8c43567fb13b68acea
Suprio-Das <priyamd507@gmail.com>
2025-07-30 23:49:13 +0600

    Delete user function created

--- controllers/Admin.js
+++ controllers/Admin.js
@@ -9,4 +9,19 @@ const GetUser = async (req, res) => {
     }
 }
 
-export { GetUser }
+const DeleteUser = async (req, res) => {
+    try {
+        const userId = req.params.id;
+        const user = await UserModel.findByIdAndDelete(userId);
+
+        if (!user) {
+            res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({ success: true, message: "User deleted successfully" })
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+export { GetUser, DeleteUser }
--- routes/AdminRouter.js
+++ routes/AdminRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { GetUser } from '../controllers/Admin.js';
+import { DeleteUser, GetUser } from '../controllers/Admin.js';
 import { isAdmin } from '../middlewares/verifyToken.js';
 
 const AdminRoutes = express.Router();
 
 AdminRoutes.get('/getuser', isAdmin, GetUser)
+AdminRoutes.post('/delete/:id', isAdmin, DeleteUser)
 
 export default AdminRoutes;
