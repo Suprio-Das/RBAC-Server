06b58b1922e6483ddf0e5bf832421ec223c15c36
Suprio-Das <priyamd507@gmail.com>
2025-07-30 22:50:00 +0600

    token not found response created

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -3,7 +3,11 @@ import jwt from 'jsonwebtoken';
 const isAdmin = async (req, res, next) => {
     try {
         const token = await req.cookies.token;
-        console.log(token);
+
+        if (!token) {
+            res.status(401).json({ message: "Unauthorized: No token found" })
+        }
+
     } catch (error) {
         console.log(error)
     }
